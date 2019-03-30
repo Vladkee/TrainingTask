c47aac33dd2d0df7cf992cf080c2f47c40ca9580
vladkee@gmail.com <vladkee@gmail.com>
2019-03-30 15:21:44 +0200

    Task 6 was added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -26,7 +26,6 @@ namespace TrainingTask
             short num2 = 1500;
 
             Console.WriteLine($"{num1} + {num2} = {Program.Sum(num1, num2)}");
-            */
             //
             //Task 5 activation.
             //
@@ -46,14 +45,21 @@ namespace TrainingTask
             // Тип decimal должен содержать число с плавающей точкой, но так как int это целочисленный тип, то при выводе он обрезает значение до целого числа.
             //
             Console.WriteLine((int)dec);
+            */
+            Task6();
         }
-
+        //
+        //Square calculation
+        //
         public static decimal PerformCalculation(int r)
         {
             const decimal Pi = 3.14m;
             decimal square = Pi * r * r;
             return square;
         }
+        //
+        //Part of task 4.
+        //
         public static int Sum(int x, int y)
         {
             return x + y;
@@ -62,6 +68,19 @@ namespace TrainingTask
         //Вывод: больший тип данных мы не можем перевести в меньший. (short > int = OK; long > int = ERROR)
         //
 
+        public static void Task6()
+        {
+            double aver;
+            int num1 = 10;
+            int num2 = 13;
+            int num3 = 15;
+            aver = (double)(num1 + num2 + num3) /3;
+            Console.WriteLine("Среднее арифметическое число " + aver);
+            //
+            //Основной проблемой является "обрезание" числа. Ср. ариф. должно выйти около 12.666. Тип int обрезает и округляет число до целых без округления в большую сторону.
+            //
+        }
+        
 
     }
 }
