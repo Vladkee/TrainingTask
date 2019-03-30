f2d18fdad4eb5e786d8dfbd43173b0acde66c617
vladkee@gmail.com <vladkee@gmail.com>
2019-03-30 15:07:26 +0200

    Task 5 was added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -18,16 +18,34 @@ namespace TrainingTask
             int.TryParse(radiusString, out int radius);
         
             Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
-            */
 
             //
-            //task 4 activation
+            //Task 4 activation.
             //
             short num1 = 1000;
             short num2 = 1500;
 
             Console.WriteLine($"{num1} + {num2} = {Program.Sum(num1, num2)}");
-            
+            */
+            //
+            //Task 5 activation.
+            //
+            int i1 = 455;
+            int i2 = 84500;
+            decimal dec = 7.98845m;
+
+            //
+            // Приводим два числа типа int к типу short.
+            // Тип данных short имеет диапазон около 32 000, поэтому в итоге мы получаем остаток числа - 18964.
+            //
+            Console.WriteLine((short)i1);
+            Console.WriteLine((short)i2);
+
+            //
+            // Приводим число типа decimal к типу int.
+            // Тип decimal должен содержать число с плавающей точкой, но так как int это целочисленный тип, то при выводе он обрезает значение до целого числа.
+            //
+            Console.WriteLine((int)dec);
         }
 
         public static decimal PerformCalculation(int r)
