027bc3e3e37609d14f32b2fcf1f1e11323524aad
vladkee@gmail.com <vladkee@gmail.com>
2019-04-01 17:53:53 +0300

    HomeTask1 was added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -10,6 +10,7 @@ namespace TrainingTask
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Hello World");
             Console.Write("Add radius ");
             string radiusString = Console.ReadLine();
@@ -17,13 +18,19 @@ namespace TrainingTask
             int.TryParse(radiusString, out int radius);
         
             Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
+            */
+            HomeTask1();
         }
-
         public static decimal PerformCalculation(int r)
         {
             const decimal Pi = 3.14m;
             decimal square = Pi * r * r;
             return square;
+        }
+        public static void HomeTask1()
+        {
+            int x1 = 5;
+            Console.WriteLine(x1);
         }
     }
 }
