010e305dd5c0dd17022fcf0e125c4f433a95de97
vladkee@gmail.com <vladkee@gmail.com>
2019-04-13 12:20:33 +0300

    Task with operations with array is added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -10,20 +10,53 @@ namespace TrainingTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Write("Add radius ");
-            string radiusString = Console.ReadLine();
-
-            int.TryParse(radiusString, out int radius);
-        
-            Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
+            Massive();
         }
 
-        public static decimal PerformCalculation(int r)
+        public static void Massive()
         {
-            const decimal Pi = 3.14m;
-            decimal square = Pi * r * r;
-            return square;
+            Console.WriteLine("Enter a size of array:");
+            var size = Int32.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            var random = new Random();
+            int sum = 0;
+            int arithmetic = 0;
+            string strOddNumbers = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 10);
+            }
+            Console.WriteLine(string.Join(",", array));
+            Console.WriteLine();
+
+            int max = array[0];
+            int min = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            Console.WriteLine($"Sum = {sum}.\n");
+
+            arithmetic = sum / size;
+            Console.WriteLine($"Arithmetic number is {arithmetic}.\n");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    strOddNumbers += array[i] + " "; 
+                }
+            }
+            Console.WriteLine($"Odd numbers: {strOddNumbers}\n");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i]) max = array[i];
+                if (max > array[i]) min = array[i];
+            }
+            Console.WriteLine($"max = {max}, min = {min}");
         }
     }
 }
