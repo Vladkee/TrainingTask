79338228c9eb0f1f72a23b71d02bf0614b359869
vladkee@gmail.com <vladkee@gmail.com>
2019-04-16 15:59:03 +0300

    Task with binary numbers is added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -10,20 +10,74 @@ namespace TrainingTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Write("Add radius ");
-            string radiusString = Console.ReadLine();
-
-            int.TryParse(radiusString, out int radius);
-        
-            Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
+            Task000111();
         }
 
-        public static decimal PerformCalculation(int r)
+        public static void Task000111()
         {
-            const decimal Pi = 3.14m;
-            decimal square = Pi * r * r;
-            return square;
+            Console.WriteLine("Enter amount of elements:");
+            var element = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter {element} elements separated by space:");
+            var arrayNumbers = Console.ReadLine();
+
+            int[] array = new int[element];
+            string[] strArrayNumbers = arrayNumbers.Split(' ');
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Int32.TryParse(strArrayNumbers[i], out array[i]);
+            }
+            //foreach (var item in array)
+            //{
+            //    Console.WriteLine(item);
+            string arrayJoin = string.Join(",", array);
+
+            int symbols = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                symbols += array[i];
+            }
+
+            string bits = "";
+            if (array[0] == 0)
+            {
+                for (int i = 1; i < element; i++)
+                {
+                    if (i % 2 == 0)
+                        for (int k = 0; k < array[i]; k++)
+                        {
+                            bits += 0;
+                        }
+                    else
+                    {
+                        for (int k = 0; k < array[i]; k++)
+                        {
+                            bits += 1;
+                        }
+                    }
+                }
+            }
+            else if (array[0] > 0)
+            {
+                for (int i = 0; i < element; i++)
+                {
+                    if (i % 2 ==0)
+                    for (int k = 0; k < array[i]; k++)
+                    {
+                        bits += 0;
+                    }
+                    else
+                    {
+                        for (int k = 0; k < array[i]; k++)
+                        {
+                            bits += 1;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Array {arrayJoin} should looks like {bits}");
         }
     }
 }
