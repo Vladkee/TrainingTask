2399d908bc40690e0cb25a69b6ad0b1fa2c95e77
vladkee@gmail.com <vladkee@gmail.com>
2019-04-20 11:00:03 +0300

    Task Calculation is added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -10,20 +10,63 @@ namespace TrainingTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Write("Add radius ");
-            string radiusString = Console.ReadLine();
-
-            int.TryParse(radiusString, out int radius);
-        
-            Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
+            PerformCalculation();
         }
 
-        public static decimal PerformCalculation(int r)
+        public static void PerformCalculation()
         {
-            const decimal Pi = 3.14m;
-            decimal square = Pi * r * r;
-            return square;
+            Console.Write("Enter the firts number: ");
+            var firstNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            var secondNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the type of calculation (+, -, *, /): ");
+            var sign = Console.ReadLine();
+            double result = 0;
+
+            switch (sign)
+            {
+                case "+":
+                    result = Sum(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    result = Sub(firstNumber, secondNumber);
+                    break;
+                case "*":
+                    result = Mul(firstNumber, secondNumber);
+                    break;
+                case "/":
+                    result = Div(firstNumber, secondNumber);
+                    break;
+                default:
+                    Console.WriteLine("You've entered wrong sign.");
+                    break;
+            }
+            Console.WriteLine($"Result = {result}");
+        }
+
+        private static double Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static double Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        private static double Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        private static double Div(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("ERROR! You can't devide by 0.");
+                return 0;
+            }
+            return a / b;
         }
     }
 }
