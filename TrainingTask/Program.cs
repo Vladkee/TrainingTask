fd19a0d5ca3b8192a444017be13e222ebc248e82
Vladkee <vladkee@gmail.com>
2019-04-23 20:26:30 +0300

    fixing

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -6,78 +6,81 @@ using System.Threading.Tasks;
 
 namespace TrainingTask
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Task000111();
-        }
 
-        public static void Task000111()
-        {
-            Console.WriteLine("Enter amount of elements:");
-            var element = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"Enter {element} elements separated by space:");
-            var arrayNumbers = Console.ReadLine();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Task000111();
+		}
 
-            int[] array = new int[element];
-            string[] strArrayNumbers = arrayNumbers.Split(' ');
+		public static void Task000111()
+		{
+			Console.WriteLine("Enter amount of elements:");
+			var element = Int32.Parse(Console.ReadLine());
+			Console.WriteLine($"Enter {element} elements separated by space:");
+			var arrayNumbers = Console.ReadLine();
 
-            Console.WriteLine();
+			int[] array = new int[element];
+			string[] strArrayNumbers = arrayNumbers.Split(' ');
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                Int32.TryParse(strArrayNumbers[i], out array[i]);
-            }
-            //foreach (var item in array)
-            //{
-            //    Console.WriteLine(item);
-            string arrayJoin = string.Join(",", array);
+			Console.WriteLine();
 
-            int symbols = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                symbols += array[i];
-            }
+			for (int i = 0; i < array.Length; i++)
+			{
+				Int32.TryParse(strArrayNumbers[i], out array[i]);
+			}
+			//foreach (var item in array)
+			//{
+			//    Console.WriteLine(item);
+			string arrayJoin = string.Join(",", array);
 
-            string bits = "";
-            if (array[0] == 0)
-            {
-                for (int i = 1; i < element; i++)
-                {
-                    if (i % 2 == 0)
-                        for (int k = 0; k < array[i]; k++)
-                        {
-                            bits += 0;
-                        }
-                    else
-                    {
-                        for (int k = 0; k < array[i]; k++)
-                        {
-                            bits += 1;
-                        }
-                    }
-                }
-            }
-            else if (array[0] > 0)
-            {
-                for (int i = 0; i < element; i++)
-                {
-                    if (i % 2 ==0)
-                    for (int k = 0; k < array[i]; k++)
-                    {
-                        bits += 0;
-                    }
-                    else
-                    {
-                        for (int k = 0; k < array[i]; k++)
-                        {
-                            bits += 1;
-                        }
-                    }
-                }
-            }
-            Console.WriteLine($"Array {arrayJoin} should looks like {bits}");
-        }
-    }
+			int symbols = 0;
+			for (int i = 0; i < array.Length; i++)
+			{
+				symbols += array[i];
+			}
+
+			string bits = "";
+			if (array[0] == 0)
+			{
+				for (int i = 1; i < element; i++)
+				{
+					if (i % 2 == 0)
+						for (int k = 0; k < array[i]; k++)
+						{
+							bits += 0;
+						}
+					else
+					{
+						for (int k = 0; k < array[i]; k++)
+						{
+							bits += 1;
+						}
+					}
+				}
+			}
+			else if (array[0] > 0)
+			{
+				for (int i = 0; i < element; i++)
+				{
+					if (i % 2 == 0)
+						for (int k = 0; k < array[i]; k++)
+						{
+							bits += 0;
+						}
+					else
+					{
+						for (int k = 0; k < array[i]; k++)
+						{
+							bits += 1;
+						}
+					}
+				}
+			}
+			Console.WriteLine($"Array {arrayJoin} should looks like {bits}");
+		}
+	}
+
+	
 }
