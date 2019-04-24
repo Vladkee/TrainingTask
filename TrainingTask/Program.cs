d04dbd71397b0d749ce12392d222c1b0de2b5d05
Vladkee <vladkee@gmail.com>
2019-04-24 21:37:04 +0300

    Task2 with products calculation is added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -11,7 +11,9 @@ namespace TrainingTask
 	{
 		static void Main(string[] args)
 		{
-			Task000111();
+			//Task000111();
+
+			Products();
 		}
 
 		public static void Task000111()
@@ -80,7 +82,40 @@ namespace TrainingTask
 			}
 			Console.WriteLine($"Array {arrayJoin} should looks like {bits}");
 		}
-	}
 
-	
+		public static void Products()
+		{
+			string[] products = new string[] { "Груши", "Яблоки", "Огурцы", "Помидоры", "Укроп", "Петрушка", "Курица", "Сыр", "Масло", "Молоко" };
+			decimal[] prices = new decimal[] { 40.50m, 31.20m, 21.80m, 35.00m, 150.00m, 145.00m, 200.00m, 400.00m, 81.40m, 81.40m };
+			decimal sumToBuy = 0.00m;
+
+			while (true)
+			{
+				Console.WriteLine("What you want to buy? If you would like to stop the process press '0'.");
+				Console.Write("Please, enter the code of a product 1-10: ");
+				string strCodeOfProduct = Console.ReadLine();
+				Console.WriteLine();
+
+				bool isStopNumber = Int32.TryParse(strCodeOfProduct, out int codeOfProduct);
+				if (codeOfProduct == 0)
+				{
+					Console.WriteLine($"You should pay {sumToBuy} gryvnias for the products.");
+					Console.WriteLine();
+					break;
+				}
+	 			else if (!isStopNumber)
+				{
+					Console.WriteLine("You've entered not a number. Try again!");
+					break;
+				}
+
+				Console.WriteLine("How many you want to buy?");
+				Console.Write("Please, enter amount of the product: ");
+				var amountOfProduct = Decimal.Parse(Console.ReadLine());
+				Console.WriteLine();
+
+				sumToBuy += prices[codeOfProduct-1] * amountOfProduct;
+			}
+		}
+	}
 }
