dcf81caff252aaf2c1bd0ccbb56e182202e7f084
Vladkee <vladkee@gmail.com>
2019-04-27 13:56:13 +0300

    Task with train scheduler is added.

---
+++ TrainingTask/Train.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingTask
+{
+	public struct Train
+	{
+		private string destination;
+
+		private int trainNumber;
+
+		private DateTime departureTime;
+
+		public Train(int trainNumber, string destination, DateTime departureTime)
+		{
+			if (trainNumber == 0)
+				throw new ArgumentException(nameof(trainNumber));
+
+			if (string.IsNullOrWhiteSpace(destination))
+				throw new ArgumentException(nameof(destination));
+
+			this.destination = destination;
+			this.trainNumber = trainNumber;
+			this.departureTime = departureTime;
+			
+		}
+
+		public int GetTrainNumber()
+		{
+			return this.trainNumber;
+		}
+
+		public string GetDestination()
+		{
+			return this.destination;
+		}
+
+		public DateTime GetDepartureTime()
+		{
+			return this.departureTime;
+		}
+	}
+}
