f635c329299e62ea5bec417c4db21a77c2f729e5
Vladkee <vladkee@gmail.com>
2019-05-04 13:59:34 +0300

    The first part of Task1 is added.

--- TrainingTask/Program.cs
+++ TrainingTask/Program.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingTask.Task1;
 
 namespace TrainingTask
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.Write("Add radius ");
-            string radiusString = Console.ReadLine();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vehicle[] vehicles =
+			{
+				new Car(4, EngineType.Diesel, GearBoxType.Auto, 4, ConsoleColor.Cyan, 200, 5),
+				new Car(2, EngineType.Petrol, GearBoxType.Manual, 4, ConsoleColor.Green, 320, 2),
+				new Bicycle(BicycleType.MountainBike, BrakeType.HandBrakes, 2, ConsoleColor.Blue, 60, 1),
+				new Bicycle(BicycleType.RoadBike, BrakeType.HandBrakes, 2, ConsoleColor.Blue, 80, 1),
+				new Moto(MotoType.Enduro, 2, ConsoleColor.DarkRed, 180, 2),
+				new Moto(MotoType.Adventure, 2, ConsoleColor.Yellow, 100, 3)
+			};
 
-            int.TryParse(radiusString, out int radius);
-        
-            Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
-        }
-
-        public static decimal PerformCalculation(int r)
-        {
-            const decimal Pi = 3.14m;
-            decimal square = Pi * r * r;
-            return square;
-        }
-    }
+			foreach (var item in vehicles)
+			{
+				item.Move(5000);
+			}
+		}
+	}
 }
+
+
+
---
+++ TrainingTask/Task1/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingTask.Task1
+{
+	class Car : Vehicle
+	{
+		public int DoorsCount { get; set; }
+		public EngineType Engine { get; set; }
+		public GearBoxType GearBox { get; set; }
+
+		public Car (int doorsCount, EngineType engine, GearBoxType gearBox, int wheelCount, ConsoleColor color, int maxSpeed, int passengerCount) 
+			:base (wheelCount, color, maxSpeed, passengerCount)
+		{
+			DoorsCount = doorsCount;
+			Engine = engine;
+			GearBox = gearBox;
+		}
+	}
+
+	enum EngineType
+	{
+		Petrol,
+		Diesel,
+		Electro
+	}
+
+	enum GearBoxType
+	{
+		Manual,
+		Auto,
+		Robot
+	}
+}
