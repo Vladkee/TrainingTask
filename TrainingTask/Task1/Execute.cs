565c5efb103136b256136004fb826aade1ce40dc
Vladkee <vladkee@gmail.com>
2019-05-07 19:41:34 +0300

    Park 3 of Task1 is added.

--- TrainingTask/Task1/Execute.cs
+++ TrainingTask/Task1/Execute.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrainingTask
+namespace TrainingTask.Task1
 {
 	static class Execute
 	{
 		public static void GetSalaryAndAge()
 		{
 			Worker worker1 = new Worker("Dima", 25, 8300.00m);
-			Worker worker2 = new Worker("Oleg", 21, 11100.00m);
+			Worker worker2 = new Worker("Oleg", 101, 11100.00m);
 
 			decimal sumSalary = worker1.Salary + worker2.Salary;
 			int sumAge = worker1.Age + worker2.Age;
