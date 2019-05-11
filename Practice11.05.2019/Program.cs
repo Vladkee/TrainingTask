047fad0fc7c10ffa685cca0d7bb57e3fc954f93e
Vladkee <vladkee@gmail.com>
2019-05-11 13:13:58 +0300

    update

--- Practice11.05.2019/Program.cs
+++ Practice11.05.2019/Program.cs
@@ -7,31 +7,7 @@ namespace Practice11._05._2019
 	{
 		static void Main(string[] args)
 		{
-			Figure[] figures =
-				{
-				new Circle(10, 55.45),
-				new Circle(4.6, 33.11),
-				new Square(4.5, 22.15),
-				new Square(12, 34.89),
-				new Triangle(5, 6.6, 7.5, 76.34),
-				new Triangle(4.2, 8.7, 4, 35.21)
-				 };
-
-			foreach (var item in figures)
-			{
-				if(item is Circle circle)
-				{
-					circle.Draw();
-				}
-				else if (item is Triangle triangle)
-				{
-					triangle.Draw();
-				}
-				else if (item is Square square)
-				{
-					square.Draw();
-				}
-			}
+			Task1.FigureDrawing.Execute();
 		}
 	}
 }
---
+++ Practice11.05.2019/Task1/FigureDrawing.cs
@@ -0,0 +1,39 @@
+﻿using Practice11._05._2019.Figures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice11._05._2019.Task1
+{
+	class FigureDrawing
+	{
+		public static void Execute()
+		{
+			Figure[] figures =
+					{
+				new Circle(10, 55.45),
+				new Circle(4.6, 33.11),
+				new Square(4.5, 22.15),
+				new Square(12, 34.89),
+				new Triangle(5, 6.6, 7.5, 76.34),
+				new Triangle(4.2, 8.7, 4, 35.21)
+				 };
+
+			foreach (var item in figures)
+			{
+				if (item is Circle circle)
+				{
+					circle.Draw();
+				}
+				else if (item is Triangle triangle)
+				{
+					triangle.Draw();
+				}
+				else if (item is Square square)
+				{
+					square.Draw();
+				}
+			}
+		}
+	}
+}
