77a6ea6ef6d41c5a0860af1cc37ff0a8fc6000f7
Vladkee <vladkee@gmail.com>
2019-05-13 21:57:47 +0300

    Some changes are added.

---
+++ Practice11.05.2019/Task2/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice11._05._2019.Task2
+{
+	class Worker
+	{
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+
+		public void Fertilize()
+		{
+
+		}
+
+		public void Water()
+		{
+
+		}
+	}
+}
